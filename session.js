b07200478b574da72c00d19b72f0c6d44320a29e
ImashaMD <imashapathum000@gmail.com>
2024-11-21 17:41:18 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=XNNSTAjB#fqgvW9V1LD6wRdSRUhq5afzFGbFM2jP9dxzV_73mllc",
 };
